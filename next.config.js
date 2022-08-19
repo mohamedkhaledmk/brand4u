e5646c736b7d25cefe2ca514457efa0f56abbc9b
mohamedkhaledmk <66388583+mohamedkhaledmk@users.noreply.github.com>
2022-08-19 13:30:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     BASE_URL: "https://hotchpotch-store.herokuapp.com",
     MONGODB_URL:
-      "mongodb+srv://drogo:1234@cluster0.g7ulmmn.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://mohamedkhaled:<password>@cluster0.hukcudg.mongodb.net/?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "0q%nmK@ZLyYBitw5myizzl@B$!dXPZ9n@X%QM^&Hnumh^YcBm1",
     REFRESH_TOKEN_SECRET: "66nd!6C6gu*nE7BgR7HqY8@XqPv@xj$&IAdE9A28r^zVv0cVmG",
     PAYPAL_CLIENT_ID:
