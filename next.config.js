5700b4a8c8a10c08d9e3e6ac357ff67245086a1b
mohamedkhaledmk <66388583+mohamedkhaledmk@users.noreply.github.com>
2022-08-20 04:57:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     BASE_URL: "https://mi5a.herokuapp.com",
-    MONGODB_URL:
+        MONGODB_URL:
       "mongodb+srv://mohamedkhaled:mi5a@cluster0.hukcudg.mongodb.net/?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "0q%nmK@ZLyYBitw5myizzl@B$!dXPZ9n@X%QM^&Hnumh^YcBm1",
     REFRESH_TOKEN_SECRET: "66nd!6C6gu*nE7BgR7HqY8@XqPv@xj$&IAdE9A28r^zVv0cVmG",
